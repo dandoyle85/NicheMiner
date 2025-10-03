20c59ea051741c1824c479b466e304d4c375cecb
dandoyle85 <danny.doyle.85@gmail.com>
2025-10-03 14:13:48 -0400

    Delete env.js

--- env.js
+++
@@ -1,5 +0,0 @@
-// Local dev helper. In Cloudflare Pages, set VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY.
-window.env = {
-  SUPABASE_URL: "https://dkinvqfnnrnzoucmbuaa.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRraW52cWZubnJuem91Y21idWFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzODM1NTMsImV4cCI6MjA3NDk1OTU1M30.uP8F3Aam8cS5SwB4AwgFYk_oQJ4TXQwq3DYGaO64xJY"
-};
